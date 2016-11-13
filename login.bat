1a811f68d95c8da6cbbbdb43b169129bb2b93dfe
Max Mayorov <max.mayorov@live.com>
2016-11-13 23:44:12 +0100

    Playing with FB / nothing works

---
+++ login.bat
@@ -0,0 +1,1 @@
+curl -i -X "GET" "https://www.facebook.com/v2.8/dialog/oauth?client_id=1699915190325737&redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=code&scope=user_managed_groups"
