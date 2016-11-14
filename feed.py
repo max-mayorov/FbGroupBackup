0c324a7bd7a4bcf52fca4b7b309c0a66c440c178
Max Mayorov <max.mayorov@boondoggle.eu>
2016-11-14 13:55:03 +0100

    Python retrieval of all feed posts

--- feed.py
+++ feed.py
@@ -1,6 +1,37 @@
 import urllib.request, json
+
+
+def getFeed(url, i):
+    print(i)
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read().decode("utf-8"))
+    
+    print(url)
+    #print(data)
+    print(i)
+
+    dict = data["data"]
+    print(len(dict))
+    if len(dict)==0:
+        return dict
+
+    #prev = data["paging"]["previous"]
+    #if prev:
+    #    dict.extend(getFeed(prev, i-1))
+    
+    next = data["paging"]["next"]
+    if next:
+        dict.extend(getFeed(next, i+1))
+    
+    return dict
+
+
 token = "EAAYKEFkRUekBALvUVuZAiF9gXpFBxj3G7MEcr07lUl1qdi1Y8EvF3ku78x54oKE9O3SsuZANI4ZBXFMYcbw0F15OpaIIP1ZAQeBxZACe7TyC9b3tZBFWr9SUovLAWs69gtLBrErrn3oMIOmpRBCw9GZBd3rYP7DJpoZD"
-url = "https://graph.facebook.com/v2.8/484923048324193?fields=feed%7Bchild_attachments%2Ccaption%2Cdescription%2Cfrom%2Cfull_picture%2Cmessage%2Cpicture%2Ctype%2Cpermalink_url%7D&access_token=" + token
-with urllib.request.urlopen(url) as response:
-    data = json.loads(response.read())
-print(data)
+feed = getFeed("https://graph.facebook.com/v2.8/484923048324193/feed?child_attachments%2Ccaption%2Cdescription%2Cfrom%2Cfull_picture%2Cmessage%2Cpicture%2Ctype%2Cpermalink_url%7D&access_token=" + token, 0) 
+
+for post in feed:
+    print(post["id"]+" "+post["message"].encode("cp866", "replace").decode("cp866"))
+
+
+
+
